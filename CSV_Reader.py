3c6411db87fde834dd392c09526b305b1762d022
Yash Mahtani <ymahtani5911@bths.edu>
2020-10-31 13:35:25 -0400

    Update CSV_Reader.py

--- CSV_Reader.py
+++ CSV_Reader.py
@@ -1,4 +1,11 @@
 import pandas as pd
 from csv import DictReader
 
-ourCSV = pd.read_csv('seinfeld_scripts.csv')
+#ourCSV = pd.read_csv('seinfeld_scripts.csv')
+
+#firstScript = ourCSV["text"]
+
+with open('seinfeld_scripts.csv', 'r') as read_obj:
+    csv_dict_reader = DictReader(read_obj)
+    for row in csv_dict_reader:
+        print(row['text'])
