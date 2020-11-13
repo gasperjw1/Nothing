9752f46abf5343cfdd273ae324d1a96238036e41
Yash Mahtani <ymahtani5911@bths.edu>
2020-11-13 14:32:08 -0500

    Updated type of scripts used in corpus and fixed some mistakes in code

--- Topic_Model_Seinfeld.py
+++ Topic_Model_Seinfeld.py
@@ -154,8 +154,8 @@ with open('seinfeld_scripts.csv', 'r') as read_obj:
 
         counter += 1
 
-        # episodeNumList.append(counter)
-        # scriptList.append(cleanedScript)
+        episodeNumList.append(counter)
+        scriptList.append(cleanedScript)
 
         t = setUp(cleanedScript)
 
@@ -187,11 +187,83 @@ with open('seinfeld_scripts.csv', 'r') as read_obj:
 ##############################################################
 
 filesForCorpus = []
+counter  = 0
 
-with open('bunchOfScripts.csv') as manyAScript:
-    for aScript in manyAScript:
-        tks = setUp(aScript)
-        filesForCorpus.append(tks)
+with open('bunchOfScripts.csv', 'r') as read_obj:
+    csv_dict_reader = DictReader(read_obj)
+
+    for row in csv_dict_reader:
+        #print(row['text'])
+
+        stop = False
+        script2 = ""
+        script3 = ""
+        script4 = ""
+        script5 = ""
+        script6 = ""
+        counterEnds = 0
+        cleanedScript = ""
+
+        #print(row['text'])
+
+        for word in row['Content'].split(' '):
+            #print(word)
+            #if "</b>" in word:
+            #    print(word)
+            #    counterEnds  = counterEnds + 1
+            #if counterEnds >= 3:
+            #    script2 = script2 + " " + word
+
+            if "<" not in word:
+                script2 = script2 + ' ' + word
+        
+        #print(script2)
+
+        for word in script2.split(' '):
+            if '(' in word:
+                stop = True
+            
+            if not stop:
+                script3 = script3 + " " + word
+
+            if ')' in word:
+                stop = False
+        
+        #print(script3)
+        
+        stop = False
+
+        for word in script3.split(' '):
+            #print(word)
+            if len(word) > 0 and "\n" not in word:
+                script4 = script4 + " " + word
+
+
+        for word in script4.split(' '):
+            tempWord = ""
+            for letter in word:
+                if letter == "." or letter == "!" or letter == "?":
+                    tempWord = tempWord + " "
+                else:
+                    tempWord = tempWord + letter
+
+            cleanedScript = cleanedScript + " " + tempWord
+
+        counter += 1
+
+        # episodeNumList.append(counter)
+        # scriptList.append(cleanedScript)
+
+        t = setUp(cleanedScript)
+
+        print(counter)
+
+        filesForCorpus.append(t)
+
+# with open('bunchOfScripts.csv') as manyAScript:
+#     for aScript in manyAScript:
+#         tks = setUp(aScript)
+#         filesForCorpus.append(tks)
 
 ##############################################################
 ##############################################################
